84c12ca7803b7eb784b4abeb760f5a07fa86e451
zhewei li <43416079+we1k@users.noreply.github.com>
2023-12-06 15:54:29 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,20 @@
+# install miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+
+# install tmux
+sudo apt-get install tmux
+mv ~/dot_files/tmux.conf ~/.tmux.conf
+
+# git clone plugin for .zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+## [optional]
+conda create -n Bert python==3.10
+conda activate Bert
+pip install -r ~/dot_files/requirements.txt
