0a1d457e9615fa065aef58c2cd3a98fdd7bde33d
zhewei li <43416079+we1k@users.noreply.github.com>
2024-05-27 15:29:58 +0800

    Update start_tmux.sh

--- start_tmux.sh
+++ start_tmux.sh
@@ -20,7 +20,9 @@ tmux split-window -v -t $SESSION_NAME
 tmux split-window -h -t $SESSION_NAME:1.1
 
 # 在第一个窗格中运行gpustat
-tmux send-keys -t $SESSION_NAME:1.1 'gpustat -i 1' C-m
+# tmux send-keys -t $SESSION_NAME:1.1 'gpustat -i 1' C-m
+tmux send-keys -t $SESSION_NAME:1.1 'nvitop -m auto' C-m
+
 
 # 在第二个窗格中运行htop
 tmux send-keys -t $SESSION_NAME:1.2 'htop' C-m
